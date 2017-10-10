60e8867f8f9929a5c2e0b9d5d4e6388afa3dedbc
Руслан Королев <techfusionwork@gmail.com>
2017-10-10 22:38:26 +0300

    init repo

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './styles/App.css'
+
+/*
+* В этом файле делается верстка
+* */
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+	      <header className="header">
+	      </header>
+	      <main>
+	      </main>
+      </div>
+    );
+  }
+}
+
+export default App;
